af490bfcff62186eea072d4882ae6f76ea6caca5
adeputraprimasuhendri <ideatechnologyindonesia@gmail.com>
2022-06-22 01:16:18 +0700

    Add dummy socket

--- app.py
+++ app.py
@@ -13,30 +13,28 @@ sock = Sock(app)
 sock.init_app(app)
 
 @sock.route('/stock')
-def stock(ws):
+def stock():
     while True:
-        data = ws.receive()
-        if data == "stock":
-            stcd = 'BBCA'
-            from_date = '1624320000'
-            to_date = '1655856000'
-            interval = '1d'
-            url = 'https://query1.finance.yahoo.com/v7/finance/download/' + stcd + '.JK?period1=' + str(
-                from_date) + '&period2=' + str(to_date) + '&interval=' + interval + '&events=history'
-            response = urllib2.urlopen(url)
-            cr = csv.reader(codecs.iterdecode(response, 'utf-8'))
-            next(cr)
-            for row in cr:
-                n = random.randint(0, 100)
-                data = {
-                    "open": int(float(row[1])),
-                    "high": int(float(row[2])),
-                    "low": int(float(row[3])),
-                    "close": int(float(row[4]))+n,
-                    "volume": int(float(row[6]))+n
-                }
-                ws.send(data)
-                time.sleep(1)
+        stcd = 'BBCA'
+        from_date = '1624320000'
+        to_date = '1655856000'
+        interval = '1d'
+        url = 'https://query1.finance.yahoo.com/v7/finance/download/' + stcd + '.JK?period1=' + str(
+            from_date) + '&period2=' + str(to_date) + '&interval=' + interval + '&events=history'
+        response = urllib2.urlopen(url)
+        cr = csv.reader(codecs.iterdecode(response, 'utf-8'))
+        next(cr)
+        for row in cr:
+            n = random.randint(0, 100)
+            data = {
+                "open": int(float(row[1])),
+                "high": int(float(row[2])),
+                "low": int(float(row[3])),
+                "close": int(float(row[4])) + n,
+                "volume": int(float(row[6])) + n
+            }
+            ws.send(data)
+            time.sleep(1)
 
 
 @app.route('/history', methods=['GET'])
